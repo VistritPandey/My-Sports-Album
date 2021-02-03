87c21817492d1fd43e2b87cf0a1c80ed2787b7ad
vistritpandey <vistritpandey@gmail.com>
2021-02-03 13:15:12 +0530

    k

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,88 +1,88 @@
 import { StatusBar } from "expo-status-bar";
 import React, { Component } from "react";
-import { StyleSheet, Text, View, FlatList, Dimensions } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  FlatList,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
 import SecondScreen from "./SecondScreen";
 
-const numColumns = 3; // researched that there are 30 teams in baseball in the US, so thought that 3 teams per column would be perfect. Can change it according to your preference
-
+const numColumns = 3;
 const WIDTH = Dimensions.get("window").width;
 
-// Don't have the data so just passed dummy numbers
-const dataList = [
-  { key: "1" },
-  { key: "2" },
-  { key: "3" },
-  { key: "4" },
-  { key: "5" },
-  { key: "6" },
-  { key: "7" },
-  { key: "8" },
-  { key: "9" },
-  { key: "10" },
-  { key: "11" },
-];
-export default class App extends Component {
-  cb() {
-    this.props.navigator.push({
-      component: SecondScreen,
-    });
-  }
+export default function App() {
+  const [images, setimages] = useState([
+    { src: require("../../assets/Logos/Angels.png"), key: "1" },
+    { src: require("../../assets/Logos/Astros.png"), key: "2" },
+    { src: require("../../assets/Logos/Athletics.png"), key: "3" },
+    { src: require("../../assets/Logos/Braves.png"), key: "4" },
+    { src: require("../../assets/Logos/Brewers.png"), key: "5" },
+    { src: require("../../assets/Logos/Diamondbacks.png"), key: "6" },
+    { src: require("../../assets/Logos/Dodgers.png"), key: "7" },
+    { src: require("../../assets/Logos/Giants.png"), key: "8" },
+    { src: require("../../assets/Logos/Indians.png"), key: "9" },
+    { src: require("../../assets/Logos/Jays.png"), key: "10" },
+    { src: require("../../assets/Logos/Mariners.png"), key: "11" },
+    { src: require("../../assets/Logos/Marlins.png"), key: "12" },
+    { src: require("../../assets/Logos/Mets.png"), key: "13" },
+    { src: require("../../assets/Logos/Orioles.png"), key: "14" },
+    { src: require("../../assets/Logos/Padres.png"), key: "15" },
+    { src: require("../../assets/Logos/Phillies.png"), key: "16" },
+    { src: require("../../assets/Logos/Pirates.png"), key: "17" },
+    { src: require("../../assets/Logos/Rays.png"), key: "18" },
+    { src: require("../../assets/Logos/Reds.png"), key: "19" },
+    { src: require("../../assets/Logos/Redsox.png"), key: "20" },
+    { src: require("../../assets/Logos/Rockies.png"), key: "21" },
+    { src: require("../../assets/Logos/Royals.png"), key: "22" },
+    { src: require("../../assets/Logos/Tigers.png"), key: "23" },
+    { src: require("../../assets/Logos/Twins.png"), key: "24" },
+    { src: require("../../assets/Logos/Whitesox.png"), key: "25" },
+    { src: require("../../assets/Logos/Yankees.png"), key: "26" },
+    { src: require("../../assets/Logos/Chicago_Cubs.png"), key: "27" },
+    { src: require("../../assets/Logos/RedSoxPrimary.png"), key: "28" },
+    { src: require("../../assets/Logos/Texas_Rangers.png"), key: "29" },
+    { src: require("../../assets/Logos/Washington_Nationals.png"), key: "30" },
+  ]);
 
-  formatData = (dataList, numColumns) => {
-    const totalRows = Math.floor(dataList.length / numColumns);
-    let totalLastRow = dataList.length - totalRows * numColumns;
-
-    while (totalLastRow !== 0 && totalLastRow !== numColumns) {
-      dataList.push({ key: "blank", empty: true });
-      totalLastRow++;
-    }
-    return dataList;
-  };
-
-  _renderItem = ({ item, index }) => {
-    if (item.empty === true) {
-      return <View style={[styles.itemStyle, styles.itemInvisible]} />;
-    }
-    return (
-      <View style={styles.item}>
-        <Text style={styles.itemText}>{item.key}</Text>
-      </View>
-    );
-  };
-
-  render() {
-    let { container, itemText } = styles;
-    return (
-      <View style={container}>
-        <FlatList
-          data={this.formatData(dataList, numColumns)}
-          renderItem={this._renderItem}
-          keyExtractor={(item, index) => index.toString()}
-          numColumns={numColumns}
-        />
-      </View>
-    );
-  }
+  return (
+    <View style={styles.container}>
+      <FlatList
+        numColumns={numColumns}
+        showsHorizontalScrollIndicator={true}
+        data={images}
+        renderItem={({ item }) => (
+          <TouchableOpacity>
+            <Image
+              source={item.src}
+              style={{
+                width: WIDTH / numColumns,
+                borderWidth: 1,
+                resizeMode: "contain",
+                flex: 1,
+                backgroundColor: "#fff",
+                alignItems: "center",
+                justifyContent: "center",
+                margin: 1,
+                height: WIDTH / numColumns,
+                borderRadius: 10,
+                borderColor: "#ff0000",
+              }}
+            />
+            <Text>Test</Text>
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 35,
-  },
-  itemText: {
-    fontSize: 50,
-    color: "#fff",
-  },
-  item: {
-    flex: 1,
-    backgroundColor: "#8c0f2e",
-    alignItems: "center",
-    justifyContent: "center",
-    margin: 1,
-    height: WIDTH / numColumns,
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: "#fff",
   },
 });
