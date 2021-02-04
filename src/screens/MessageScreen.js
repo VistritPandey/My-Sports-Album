b1228e85837c104dc08e3a52353d94521d07f870
vistritpandey <vistritpandey@gmail.com>
2021-02-04 12:39:21 +0530

    World chat woring

--- src/screens/MessageScreen.js
+++ src/screens/MessageScreen.js
@@ -1,9 +1,8 @@
 // @refresh reset
-import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect, useCallback } from "react";
 import { GiftedChat } from "react-native-gifted-chat";
 import AsyncStorage from "@react-native-community/async-storage";
-import { StyleSheet, View, YellowBox, Button, TextInput } from "react-native";
+import { StyleSheet, View, Button, TextInput } from "react-native";
 import "firebase/firestore";
 import firebase from "firebase";
 
@@ -25,5 +24,84 @@ const db = firebase.firestore();
 const chatsRef = db.collection("chats");
 
 export default MessageScreen = () => {
-  return <View></View>;
+  const [user, setUser] = useState(null);
+  const [name, setName] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    readUser();
+    const unsubscribe = chatsRef.onSnapshot((querySnapshot) => {
+      const messagesFirestore = querySnapshot
+        .docChanges()
+        .filter(({ type }) => type === "added")
+        .map(({ doc }) => {
+          const message = doc.data();
+          return { ...message, createdAt: message.createdAt.toDate() };
+        })
+        .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime());
+      appendMessages(messagesFirestore);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  const appendMessages = useCallback(
+    (messages) => {
+      setMessages((previousMessages) =>
+        GiftedChat.append(previousMessages, messages)
+      );
+    },
+    [messages]
+  );
+
+  async function readUser() {
+    const user = await AsyncStorage.getItem("user");
+    if (user) {
+      setUser(JSON.parse(user));
+    }
+  }
+
+  async function handlePress() {
+    const _id = Math.random().toString(36).substring(7);
+    const user = { _id, name };
+    await AsyncStorage.setItem("user", JSON.stringify(user));
+    setUser(user);
+  }
+
+  async function handleSend(messages) {
+    const writes = messages.map((m) => chatsRef.add(m));
+    await Promise.all(writes);
+  }
+
+  if (!user) {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter name"
+          value={name}
+          onChangeText={setName}
+        />
+        <Button onPress={handlePress} title="Enter the chat" color="red" />
+      </View>
+    );
+  }
+  return <GiftedChat messages={messages} user={user} onSend={handleSend} />;
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 30,
+  },
+  input: {
+    height: 50,
+    width: "100%",
+    borderWidth: 1,
+    padding: 15,
+    borderColor: "gray",
+    marginBottom: 20,
+  },
+});
