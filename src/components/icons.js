19efee2ab53efe17bc71b0d8b742d4a169079373
vistritpandey <vistritpandey@gmail.com>
2021-06-14 08:39:30 +0530

    k

---
+++ src/components/icons.js
@@ -0,0 +1,10 @@
+export const baseball = require("../../assets/nav_icons/baseball.png");
+export const mlb = require("../../assets/nav_icons/mlb.png");
+export const settings = require("../../assets/nav_icons/settings.png");
+export const message = require("../../assets/nav_icons/message.png");
+export default {
+  baseball,
+  mlb,
+  settings,
+  message,
+};
